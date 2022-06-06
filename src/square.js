226a7e0629020d23df96d78876a98a4428c263f1
JayHaji <jaymainframe@JayHajis-MacBook-Pro.local>
2022-06-06 12:12:11 -0400

    Change Y to M

--- src/square.js
+++ src/square.js
@@ -20,7 +20,7 @@ export class Square extends React.Component {
             <button onClick={this.handleClick} value="A" id="square3">A</button>
            </div>
            <div className="lineCont">
-            <button onClick={this.handleClick} value="Y" id="square4">Y</button>
+            <button onClick={this.handleClick} value="M" id="square4">M</button>
             <button onClick={this.handleClick} value="S" id="square5">S</button>
             <button onClick={this.handleClick} value="R" id="square6">R</button>
             </div>
